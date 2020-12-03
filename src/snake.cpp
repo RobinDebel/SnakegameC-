5782f9437f70dafb9078ac0baef34b678f4c207a
Robin Debel <robindebel@hotmail.nl>
2020-12-03 11:59:53 +0100

    made snake.cpp

--- src/snake.cpp
+++ src/snake.cpp
@@ -17,5 +17,38 @@ void Snake::render(Canvas*canvas){
 void Snake::update(void){
     int x = 0;
     int y = 0;
+    next_position(&x,&y);
     Entity::move(x,y);
+}
+
+void Snake::next_position(int * x, int * y){
+    *x= this->x();
+    *y= this->y();
+    switch(direction){
+        case Direction::LEFT: *x = this->x() - 1; break;
+        case Direction::RIGHT: *x = this->x() + 1; break;
+        case Direction::UP: *y = this->y() - 1; break;
+        case Direction::DOWN: *y = this->y() + 1; break;
+    }    
+
+}
+
+void Snake::down(void){
+    move(Direction::DOWN);
+}
+
+void Snake::up(void){
+    move(Direction::UP);
+}
+
+void Snake::left(void){
+    move(Direction::LEFT);
+}
+
+void Snake::right(void){
+    move(Direction::RIGHT);
+}
+
+void Snake::move(Direction direction){
+    this->direction = direction;
 }
